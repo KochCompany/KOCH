60041ba2658fd27f8fe49b4b7847563f59f39ef7
antoanyanev5 <antoan.yanev5@gmail.com>
2017-12-06 07:42:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+//KOCH
+
 require('./tools.js')();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const broadcast = client.createVoiceBroadcast();
 
 let fs = require('fs');
 let request = require("request");
 const unirest = require('unirest');
 const settings = require('./settings.json');
-const wUrl = "http://api.openweathermap.org/data/2.5/weather?id=727011&appid=3d5632822352c9cd93370a8212356d3f";
 const ytdl = require('ytdl-core');
 
 const streamOptions = {
 	seek: 0,
 	volume: 1
 };
-
-const broadcast = client.createVoiceBroadcast();
 
 let last = [];
 
@@ -36,44 +36,44 @@ client.on('guildMemberRemove', member => {
 client.on('message', message => {
 	client.setMaxListeners(1);
 
-	if(message.author.bot) {
+	if (message.author.bot) {
 		return;
 	}
 
 	let res = betterEval(message.content);
 
-	if(res != undefined) {
+	if (res != undefined) {
 		message.channel.send(res);
 	}
-	
+
 	//autoresponder:
 
-	for(var k in settings.triggers) {
-		if(message.content == k) {
-			if(settings.triggers[k].startsWith("https://")) {
+	for (var k in settings.triggers) {
+		if (message.content == k) {
+			if (settings.triggers[k].startsWith("http")) {
 				message.channel.send({file: settings.triggers[k]});
-			} else {
-				message.channel.send(settings.triggers[k]);
+				return;
 			}
-			break;
+			message.channel.send(settings.triggers[k]);
+			return;
 		}
 	}
 
 	//commands:
 
-	if(message.content.startsWith(settings.prefix + 'help')) {
-		let result = "";
-		for(var i = 0; i < settings.triggers.length; i++) {
-			result += settings.triggers[i] + ' -> ' + settings.responses[i] + '\n';
-		}
-		message.author.send(result);
-	} else if(message.content.startsWith(settings.prefix + "leave")) {
+	if (message.content.startsWith(settings.prefix + 'help')) {
+		let info = "$weather \n$addResponse {trigger} {response} \n$removeResponse {trigger} \n$quote {name} \
+			\n$showQuotes {name} \n$addQuote {name} {quote} \n$removeQuote {name} -> $select {number}"
+		message.author.send(info);
+	} else if (message.content.startsWith(settings.prefix + "gn")) {
+		message.channel.send("leka kurwi");
+	} else if (message.content.startsWith(settings.prefix + "leave")) {
 		const channel = message.member.voiceChannel;
 		channel.leave();
-	} else if(checkSong(message)) {
+	} else if (checkSong(message)) {
 		const channel = message.member.voiceChannel;
 
-		if(channel == undefined) {
+		if (channel == undefined) {
 			return;
 		}
 
@@ -87,69 +87,72 @@ client.on('message', message => {
 		broadcast.on("end", () => {
 			channel.leave();
 		});
-	} else if(message.content == "zdr") {
+	} else if (message.content == "zdr") {
 		message.channel.send("zdr, " + message.author);
-	} else if(message.content == settings.prefix + "weather") {
+	} else if (message.content == settings.prefix + "weather") {
+		const wUrl = "http://api.openweathermap.org/data/2.5/weather?id=727011&appid=3d5632822352c9cd93370a8212356d3f";
 		request({
 			url: wUrl,
 			json: true
-		}, function (error, response, body) {
+		}, function(error, response, body) {
 			if (!error && response.statusCode === 200) {
 				let temp = (body.main.temp - 273.15);
 				let zaprqnkata = client.emojis.find("name", "zaprqnkata");
-				if(temp <= -10) {
+				if (temp <= -10) {
 					message.channel.send("aktualen klimat: " + temp + " °C \n'96 golemiq sneg " + zaprqnkata);
 				} else {
-					
 					message.channel.send("aktualen klimat: " + temp + " °C " + zaprqnkata);
 				}
 			}
 		})
-	} else if(message.content.startsWith(settings.prefix + 'addResponse')) {
+	} else if (message.content.startsWith(settings.prefix + 'addResponse')) {
 		let result = message.content.substring(13).split(",");
 		let response = "";
 		console.log(result, message.author.username);
-		for(let i = 1; i < result.length; i++) {
+		for (let i = 1; i < result.length; i++) {
 			response += result[i];
-			if(i < result.length - 1) {
+			if (i < result.length - 1) {
 				response += ",";
 			}
 		}
 		addResponse(result[0], response);
-	} else if(message.content.startsWith(settings.prefix + "removeResponse")) { 
+	} else if (message.content.startsWith(settings.prefix + "removeResponse")) {
+		console.log(message.content.substring(16), message.author.username);
 		removeResponse(message.content.substring(16));
-	} else if(message.content.startsWith(settings.prefix + 'setgame')) {		
+	} else if (message.content.startsWith(settings.prefix + 'setgame')) {
 		let modRole = message.guild.roles.find("name", "pulen pedal");
 		let ownerRole = message.guild.roles.find("name", "nai-golemiq pulen pedal");
-		if(message.member.roles.has(modRole.id) || message.member.roles.has(ownerRole.id)) {
+		if (message.member.roles.has(modRole.id) || message.member.roles.has(ownerRole.id)) {
 			let result = message.content.substring(9);
 			client.user.setGame(result);
+			settings.game = result;
 			saveJSON();
 		} else {
 			message.channel.send("Not authorized ;(");
 		}
-	} else if(message.content.startsWith(settings.prefix + "addQuote")) {
+	} else if (message.content.startsWith(settings.prefix + "addQuote")) {
 		let result = message.content.substring(10).split(",");
 		addQuote(result[0], result[1]);
-	} else if(message.content.startsWith(settings.prefix + "removeQuote")) {
+		console.log(result, message.content.author);
+	} else if (message.content.startsWith(settings.prefix + "removeQuote")) {
 		let result = message.content.substring(13);
 		message.channel.send(printQuotes(result));
-	} else if(message.content.startsWith(settings.prefix + "select")) {
+		console.log(result, message.content.author);
+	} else if (message.content.startsWith(settings.prefix + "select")) {
 		let result = message.content.substring(8);
 		let ret = removeQuote(eval(result));
-		if(ret == 0) {
+		if (ret == 0) {
 			message.channel.send("nema takaf index");
-		} else if(ret == -1) {
+		} else if (ret == -1) {
 			message.channel.send("kwo izbirash ti e?");
 		}
-	} else if(message.content.startsWith(settings.prefix + "showQuotes")) {
+	} else if (message.content.startsWith(settings.prefix + "showQuotes")) {
 		let result = message.content.substring(12);
 		message.channel.send(printQuotes(result));
 	} else if(message.content.startsWith(settings.prefix + "quote")) {
 		let name = message.content.split(" ");
 		message.channel.send(quote(name[1], name[2]));
 	}
-	
 });
 
 client.login(settings.token);
